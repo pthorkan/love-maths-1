9c189a4bf74649886f05890169a787856c9acbe7
pthorkan <pthorkan@gmail.com>
2022-06-28 10:19:35 +0000

    Add increment score function

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,11 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert("Hey you got it right!")
+        alert("Hey you got it right!");
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -75,11 +77,22 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrementScore () {
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
 
+        let oldScore = parseInt(document.getElementById("score").innerText);
+        document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current incorrect score from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+        document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
